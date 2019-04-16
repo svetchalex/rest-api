406f9056de8836e1ea0ca61ae50f0047bee94945
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-04-16 17:31:37 +0300

    RestAPI

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 
 function rest()
 {
-   // header('Content-Type: application/json; charset=UTF-8');
+    // header('Content-Type: application/json; charset=UTF-8');
     $url = $_SERVER['REQUEST_URI'];
     $urls = explode('/', $url);
     $result = '';
@@ -36,11 +36,17 @@ function rest()
                 create_client();
 
             }
-            if ($urls[3] === 'update' && count($urls) === 5 && is_numeric($urls[4])) {
+            if ($urls[3] === 'update'
+                && count($urls) === 5
+                && is_numeric($urls[4]
+                )) {
                 header('HTTP/1.1 200 OK');
                 update_client($urls[4]);
             }
-            if ($urls[3] === 'delete' && count($urls) === 5 && is_numeric($urls[4])) {
+            if ($urls[3] === 'delete'
+                && count($urls) === 5
+                && is_numeric($urls[4]
+                )) {
                 header('HTTP/1.1 200 OK');
                 delete_client($urls[4]);
             }
@@ -54,12 +60,15 @@ function rest()
                         header('HTTP/1.1 200 OK');
                         add_turnover($urls[3]);
                     }
-                    if ($urls[5] === 'delete' && count($urls)===7 && is_numeric($urls[6])) {
+                    if ($urls[5] === 'delete'
+                        && count($urls) === 7
+                        && is_numeric($urls[6]
+                        )) {
                         header('HTTP/1.1 200 OK');
                         delete_turnover($urls[3], $urls[6]);
                     }
                 }
-                if($urls[4]==='bonus'){
+                if ($urls[4] === 'bonus') {
                     if ($urls[5] === 'select') {
                         header('HTTP/1.1 200 OK');
                         $result = json_encode(select_bonus($urls[3]));
@@ -68,17 +77,65 @@ function rest()
                         header('HTTP/1.1 200 OK');
                         add_bonus($urls[3]);
                     }
-                    if ($urls[5] === 'update') {
+                    if ($urls[5] === 'update'
+                        && count($urls) === 7
+                        && is_numeric($urls[6]
+                        )) {
                         header('HTTP/1.1 200 OK');
-                        update_bonus($urls[3]);
+                        update_bonus($urls[3], $urls[6]);
                     }
-                    if ($urls[5] === 'delete' && count($urls)===7 && is_numeric($urls[6])) {
+                    if ($urls[5] === 'delete'
+                        && count($urls) === 7
+                        && is_numeric($urls[6]
+                        )) {
                         header('HTTP/1.1 200 OK');
                         delete_bonus($urls[3], $urls[6]);
                     }
                 }
+                if ($urls[4] === 'discount') {
+                    if ($urls[5] === 'select') {
+                        header('HTTP/1.1 200 OK');
+                        $result = json_encode(select_discount($urls[3]));
+                    }
+                    if ($urls[5] === 'add') {
+                        header('HTTP/1.1 200 OK');
+                        add_discount($urls[3]);
+                    }
+                    if ($urls[5] === 'update'
+                        && count($urls) === 7
+                        && is_numeric($urls[6]
+                        )) {
+                        header('HTTP/1.1 200 OK');
+                        update_discount($urls[3], $urls[6]);
+                    }
+                    if ($urls[5] === 'delete'
+                        && count($urls) === 7
+                        && is_numeric($urls[6]
+                        )) {
+                        header('HTTP/1.1 200 OK');
+                        delete_discount($urls[3], $urls[6]);
+                    }
+                }
             }
 
+        }
+        if ($urls[2] === 'turnover'
+            && $urls[3] === 'select'
+        ) {
+            header('HTTP/1.1 200 OK');
+            $result = json_encode(turnover_all());
+        }
+        if ($urls[2] === 'bonus'
+            && $urls[3] === 'select'
+        ) {
+            header('HTTP/1.1 200 OK');
+            $result = json_encode(bonus_all());
+        }
+        if ($urls[2] === 'discount'
+            && $urls[3] === 'select'
+        ) {
+            header('HTTP/1.1 200 OK');
+            $result = json_encode(discount_all());
         }
 
     }
@@ -86,10 +143,12 @@ function rest()
     return $result;
 }
 
-function connection(){
-   return $mysqli = new mysqli('localhost', 'stud03', 'password', 'card');
+function connection()
+{
+    return $mysqli = new mysqli('localhost', 'stud03', 'password', 'card');
 
 }
+
 function select_telephone($telephone)
 {
 
@@ -255,11 +314,12 @@ SQL;
     }
     return true;
 }
-function delete_turnover($client, $turnover)
+
+function delete_turnover($client, $id)
 {
     $mysqli = connection();
     $sql = <<<SQL
-        DELETE FROM turnover_card WHERE id_client = '$client' AND id = '$turnover'
+        DELETE FROM turnover_card WHERE id_client = '$client' AND id = '$id'
 SQL;
     try {
         if (!$mysqli->query($sql)) {
@@ -271,4 +331,209 @@ SQL;
     }
     return true;
 }
+
+function select_bonus($client)
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        SELECT * FROM bonuses WHERE id_client = '$client' ORDER BY date_bonus
+SQL;
+    try {
+        if (!$res = $mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return $res->fetch_all();
+}
+
+function add_bonus($client)
+{
+    $balance = $_POST['balance'];
+    $status = $_POST['status'];
+    $date = $_POST['date_bonus'];
+    $mysqli = connection();
+    $sql = <<<SQL
+        INSERT INTO bonuses (id, balance, status, date_bonus, id_client) VALUES
+        (null, '$balance', '$status', '$date', '$client' )
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function update_bonus($client, $id)
+{
+    $balance = $_POST['balance'];
+    $status = $_POST['status'];
+    $date = $_POST['date_bonus'];
+    $mysqli = connection();
+    $sql = <<<SQL
+        UPDATE bonuses SET balance = '$balance', status = '$status', date_bonus = '$date'  
+        WHERE id = $id AND id_client = $client
+        
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function delete_bonus($client, $id)
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        DELETE FROM bonuses WHERE id_client = '$client' AND id = '$id'
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function select_discount($client)
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        SELECT * FROM discounts WHERE id_client = '$client' ORDER BY date_discount
+SQL;
+    try {
+        if (!$res = $mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return $res->fetch_all();
+}
+
+function add_discount($client)
+{
+    $current = $_POST['current'];
+    $new = $_POST['new'];
+    $date = $_POST['date_discount'];
+    $mysqli = connection();
+    $sql = <<<SQL
+        INSERT INTO discounts (id, current, new, date_discount, id_client) VALUES
+        (null, '$current', '$new', '$date', '$client' )
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function update_discount($client, $id)
+{
+    $current = $_POST['current'];
+    $new = $_POST['new'];
+    $date = $_POST['date_discount'];
+    $mysqli = connection();
+    $sql = <<<SQL
+        UPDATE discounts SET current = '$current', new = '$new', date_discount = '$date'  
+        WHERE id = $id AND id_client = $client
+        
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function delete_discount($client, $id)
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        DELETE FROM discounts WHERE id_client = '$client' AND id = '$id'
+SQL;
+    try {
+        if (!$mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return true;
+}
+
+function turnover_all()
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        SELECT * FROM turnover_card ORDER BY date_receipt
+SQL;
+    try {
+        if (!$res = $mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return $res->fetch_all();
+}
+
+function bonus_all()
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        SELECT * FROM bonuses ORDER BY date_bonus
+SQL;
+    try {
+        if (!$res = $mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return $res->fetch_all();
+}
+function discount_all()
+{
+    $mysqli = connection();
+    $sql = <<<SQL
+        SELECT * FROM discounts ORDER BY date_discount
+SQL;
+    try {
+        if (!$res = $mysqli->query($sql)) {
+            throw new Exception($mysqli->error);
+        }
+
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+    return $res->fetch_all();
+}
+
 echo rest();
